3b360a33b6780d9864b7b3d1a2090945ef0990f7
Thomas Woltjer <thomas@thomaswoltjer.com>
2021-10-16 11:11:15 -0400

    Initial viewer

---
+++ src/OneTooCalendar/OneTooCalendar/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OneTooCalendar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var oneTooCalLoader = new OneTooCalLoader();
+        }
+    }
+
+    public class OneTooCalLoader : Window
+    {
+        public OneTooCalLoader()
+        {
+            
+        }
+    }
+}
